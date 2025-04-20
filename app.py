7afca07790040df6fbb414e3e9593dccede200ea
Robert Richard <111211180+robertrichard86@users.noreply.github.com>
2025-04-20 02:24:00 -0300

    app.py

--- app.py
+++ app.py
@@ -98,5 +98,10 @@ def adicionar():
     
     return jsonify({'success': True})
 
+import os
+
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=port)
+
